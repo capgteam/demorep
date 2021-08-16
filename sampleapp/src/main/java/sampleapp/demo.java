e6d324dfe75cd0ab866d5f59225baa0b2b06be7a
capgteam <capgteam22020@gmail.com>
2021-08-16 12:55:17 +0530

    commit all

---
+++ sampleapp/src/main/java/sampleapp/demo.java
@@ -0,0 +1,13 @@
+package sampleapp;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println("Sample App");
+		System.out.println("Sample App");
+		System.out.println("Sample App");
+		System.out.println("Sample App");
+		
+	}
+
+}
